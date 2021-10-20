3bf2511450af85424d73b8e1a03cac25275753d4
Anna Wolska <a_wl@wp.pl>
2021-10-20 18:53:52 +0200

    nie działają 3 funkcjonalnośći: dodawanie zdjęć do bloga, rejestrowanie nowego uzytkowanika, wylogowywanie

--- kotowisko/urls.py
+++ kotowisko/urls.py
@@ -33,7 +33,9 @@ urlpatterns = [
     path('user-blogs/', views.UserBlogs.as_view()),
     path('blog-delate/<int:id>/',views.DelateBlog.as_view()),
     path('article-delate/<int:id>/',views.DelateArticle.as_view()),
-    # path('add-photo/<int:id_blog>', views.AddPhoto.as_view()),
-    # path('accounts/logout/', views.Home.as_view())
+    path('add-photo/<int:id>/', views.AddPhoto.as_view()),
+    path('registration/', views.Register.as_view()),
+    path('accounts/logout/',views.Logout.as_view()),
+    path('register-confirm/', views.RegisterConfirm.as_view())
 
 ]
